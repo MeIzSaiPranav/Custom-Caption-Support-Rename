d120b76b732630d5487125bff67f7ee2bb220b74
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2021-05-19 07:12:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,16 +1,20 @@
-import pyrogram
-
+from pyrogram import Client
+from sample_config import Config
 from plugins.help_text import rename_cb, cancel_extract
-from plugins.rename_file import force_name
+from plugins.rename_file import force_name, msg
 
 
-@pyrogram.Client.on_callback_query()
+@Client.on_callback_query()
 async def cb_handler(bot, update):
         
     if "rename_button" in update.data:
         await update.message.delete()
-        await force_name(bot, update.message)
-        
+        await force_name(bot, update.message)     
     elif "cancel_e" in update.data:
         await update.message.delete()
         await cancel_extract(bot, update.message)
+    elif "chnl1" in update.data:
+        await update.msg.copy_message(
+            chat_id=Config.CHANNEL_ID_1
+        )
+ 
