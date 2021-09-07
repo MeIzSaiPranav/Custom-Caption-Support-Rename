a8fe773ae1496a1cdb5d075be45ad1e81452a3b6
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-07 10:38:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,7 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact DEV ⭕️", url="https://t.me/prgofficial")]]),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact DEV ⭕️", url="https://t.me/Me_Iz_Mad_Boi")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
